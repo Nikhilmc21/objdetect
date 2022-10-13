5073b139c586c6885905b740a58d361a7362a73b
Nikhilmc21 <84916563+Nikhilmc21@users.noreply.github.com>
2022-10-13 19:59:01 +0530

    Add files via upload

---
+++ humanpg.js
@@ -0,0 +1,40 @@
+img="";
+status_m="";
+
+function preload(){
+    img=loadImage('download (2).jpeg');
+}
+
+function setup(){
+    canvas = createCanvas(640,450);
+    canvas.center();
+    a = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML ="Detecting...";
+}
+
+function modelLoaded(){
+    console.log("MODEL LOADED BEEP BOOP 🤖🤖🤖");
+    status_m=true;
+    a.detect(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log("ERROR ERROR 🤖🤖🤖"+error);
+    }
+    else{
+        console.log("SUCCESS SUCCESS 🤖🤖🤖");
+        console.log(results);
+        document.getElementById("detected").innerHTML= results;
+    }
+}
+
+
+function draw(){
+    image(img , 0, 0, 640, 420);
+}
+
+
+function home(){
+    window.location="index.html";
+}
---
+++ main.js
@@ -0,0 +1,9 @@
+function humanpagesend(){
+    window.location="humanpg.html";
+}
+function objectpagesend(){
+    window.location="objectpg.html";
+}
+function animalpagesend(){
+    window.location="animalpg.html";
+}
